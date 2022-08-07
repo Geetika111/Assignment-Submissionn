a254312d5961631e3dab94e834565f3ea1920786
Geetika111 <87661071+Geetika111@users.noreply.github.com>
2022-08-07 15:20:41 +0530

    Add files via upload

---
+++ Assignment-01.c
@@ -0,0 +1,99 @@
+// ques-01 write a program to print hello student on the screen 
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello Students");
+//     return 0;
+// }
+// Ques-02 wap to print hello in first  line and student in the second line
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello\nStudents");
+//     return 0;
+// }
+// Ques-03 wap to print mysirg in doble quotes
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\"MySirG\"");
+//     return 0;
+// }
+// ques-04 wap to find area of the circle 
+// #include<stdio.h>
+// int main()
+// {
+//     float r, area;
+//     printf("enter the radius of the circle:");
+//     scanf("%f",&r);
+//     area=3.14*r*r;
+//     printf("Area-%f",area);
+//     return 0;
+// }
+// ques -05 wap to calculate the length of the string using printf function
+// #include<stdio.h>
+// int main()
+// {
+//   int a;
+//   a=printf("geetikasharma");
+  
+//   printf("%d",a);
+//   return 0;
+// }
+// ques -06 wap to print the name of the user in the double quotes
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\"Hello, Geetika Sharma\"");
+//     return 0;
+// }
+// ques -07 wap to print %d on the screen
+// #include<stdio.h>
+// int main()
+// {
+//     printf("%%d");
+//     return 0;
+// }
+// ques-08 wap to print \n on the screen
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\\n");
+//     return 0;
+// }
+// ques -09 wap to print \\ on the screen
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\\\\");
+//     return 0;
+// }
+//ques -10 wap to take the date in a particular format 
+// #include<stdio.h>
+// int main()
+// {
+//     int date,month,year;
+//     printf("enter the date/month/year:");
+//     scanf("%d/%d/%d",&date,&month,&year);
+//     printf("Day-%d,Month-%d,Year-%d",date,month,year);
+//     return 0;
+// }
+// ques -11 wap to take in a format and convert it in a particular format
+// #include<stdio.h>
+// int main()
+// {
+//     int HH,MM;
+//     printf("enter the hour and the minutes HH:MM");
+//     scanf("%d:%d",&HH,&MM);
+//     printf("%d hours %d Minutes",HH,MM);
+//     return 0;
+// }
+//ques-12 Find the output of the program 
+// #include<stdio.h>
+// int main()
+// { 
+// int x = printf(“ineuron”);
+// printf(“%d”,x);
+// return 0;
+// }
+// output:-ineuron7
---
+++ Assignment-04.c
@@ -0,0 +1,126 @@
+// ques-01 write a program to print my sirg
+// #include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<=5)
+//     {
+//         printf("My SirG\n");
+//         i++;
+//     }
+//     return 0;
+// }
+
+// Ques-02 print 10 natural numbers on the screen
+// #include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<=10)
+//     {
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// ques 03 print 10 natural numbers in reverse order
+// #include<stdio.h>
+// int main()
+// {
+//     int i=10;
+//     while(i>=1)
+//     {
+//         printf("%d\n",i);
+//         i--;
+//     }
+//     return 0;
+// }
+
+// ques-04 write a program to print 10 odd natural numbers
+// #include<stdio.h>
+// int main(){
+//     int i =1;
+//     while(i<=10)
+//     {
+//         printf("%d\n",2*i-1);
+//         i++;
+//     }
+//     return 0;
+// }
+// ques 05 print 10 odd natural number in reverse order
+// #include<stdio.h>
+// int main()
+// {
+//     int i=10;
+//     while(i>=1)
+//     {
+//        printf("%d\n",2*i-1); 
+//        i--;
+//     }
+//     return 0;
+// }
+
+//  ques-06 write a program to print first 10 even natural number 
+// #include<stdio.h>
+// int main()
+// {
+//     int i=2;
+//     while(i<=10)
+//     {
+//         printf("%d\n",2*i-2);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// write a program to print 10 even natural number in revers __ORDER_BIG_ENDIAN__
+// #include<stdio.h>
+// int main()
+// {
+//     int i=10;
+//     while(i>1)
+//     {
+//         printf("%d\n",2*i-2);
+//         i--;
+//     }
+//     return 0;
+// }
+// Ques 08 -Write a program to print square of first 10 natural number
+// #include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<=10)
+//     {
+//         printf("%d\n",i*i);
+//         i++;
+//     }
+//     return 0;
+// }
+ 
+// ques -09 write a program to print cubes of first 10 natural numbers
+// #include<stdio.h>
+//     int main()
+//     {
+//     int i=1;
+//     while(i<=10)
+//     {
+//         printf("%d\n",i*i*i);
+//         i++;
+// //     }
+// //     return 0;
+// // }
+ 
+// ques 10 -write a program to print a table of 5
+//  #include<stdio.h>
+//  int main()
+//  {
+//     int i=1;
+//     while(i<=10)
+//     {
+//         printf("%d\n",5*i);
+//         i++;
+//     }
+//     return 0;
+//  }
