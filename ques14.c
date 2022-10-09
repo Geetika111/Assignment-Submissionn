09de72b2ae54fcc71cce57bcd33f81ea387c07a0
Geetika Sharma <87661071+Geetika111@users.noreply.github.com>
2022-10-09 16:39:51 +0530

    Create ques14.c

---
+++ ques14.c
@@ -0,0 +1,23 @@
+// ques-14
+// #include <stdio.h>
+// #include <conio.h>
+// int main()
+// {
+//     int i, j;
+//     for (i = 0; i <= 5; i++)
+//     {
+//         for (j = 0; j <= 5; j++)
+//         {
+//             if (j >= 1 && j <= i)
+//             {
+//                 i == 3 && j == 2 || i == 4 && j == 2 || i == 4 && j == 3 ? printf("  ") : printf(" *");
+//             }
+//             else
+//             {
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
